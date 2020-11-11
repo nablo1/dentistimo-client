5eae638bdd3726cf0941230950c1aed66a3cb2ab
livm <livm@student.chalmers.se>
2020-11-11 20:35:50 +0100

    Bootstrap-vue dependency installed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+// Bootstrap imports
+import {
+  BootstrapVue,
+  BootstrapVueIcons,
+} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Use bootstrap for styling the ui
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
