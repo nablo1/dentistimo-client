ec3f2212137dc75e05718d805042d56e10d237c7
livm <livm@student.chalmers.se>
2020-11-16 18:35:17 +0100

    Base of admin dashboard view component

---
+++ src/views/AdminDashboard/AdminDashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="admin-dashboard"></div>
+</template>
+
+<script>
+export default {
+name: "AdminDashboard"
+}
+</script>
+
+<style scoped>
+@import "./AdminDashboard.css"
+</style>
