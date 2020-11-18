cab6a74bf016655c8d3d027285321ff77883da59
livm <livm@student.chalmers.se>
2020-11-18 13:26:41 +0100

    Content component for users about to sign in from the header sidebar

---
+++ src/components/AppHeader/SigningInContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-card>
+    <b-card-body>
+      <h4>
+        Sign in
+      </h4>
+
+      <!-- TODO: Create sign in form component -->
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+  export default {
+    name: 'SidebarContentSignIn',
+  }
+</script>
+
+<style scoped></style>
