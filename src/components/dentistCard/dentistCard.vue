4154c68ce7348f78a964f283c6665af52b8b6877
okeyo <okeyo@student.chalmers.se>
2020-11-18 15:55:49 +0100

    Put the card as a stretched link

--- src/components/dentistCard/dentistCard.vue
+++ src/components/dentistCard/dentistCard.vue
@@ -3,34 +3,38 @@
     <b-card
       no-body
       class="overflow-hidden"
-      style="max-width: 350px; max-height:206px"
+      style="max-width: 350px;
+        max-height:206px"
     >
-      <b-row no-gutters>
-        <b-col md="6">
-          <!--Placeholder image -->
-          <b-card-img
-            src="dentistImage..jpg"
-            alt="Image"
-            class="rounded-0"
-          ></b-card-img>
-        </b-col>
-        <b-col md="6">
-          <b-card-body title="">
-            <b-card-text class="font-weight-bold">
-              <br />
+      <!--Placeholder link -->
+      <a class="stretched-link " href>
+        <b-row no-gutters>
+          <b-col md="6">
+            <!--Placeholder image -->
+            <b-card-img
+              src="dentistImage..jpg"
+              alt="Image"
+              class="rounded-0"
+            ></b-card-img>
+          </b-col>
+          <b-col md="6">
+            <b-card-body title="">
+              <b-card-text class="font-weight-bold">
+                <br />
 
-              <!-- Placeholder -->
-              Dentist Clinic Name
-            </b-card-text>
-            <b-card-text class="text-left">
-              <b-icon icon="geo-alt-fill"></b-icon>
+                <!-- Placeholder -->
+                Dentist Clinic Name
+              </b-card-text>
+              <b-card-text class="text-left">
+                <b-icon icon="geo-alt-fill"></b-icon>
 
-              <!--Placeholder -->
-              Location
-            </b-card-text>
-          </b-card-body>
-        </b-col>
-      </b-row>
+                <!--Placeholder -->
+                Location
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </a>
     </b-card>
   </div>
 </template>
