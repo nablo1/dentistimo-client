e74c281b184fc3868f0d8472e750f38e0aa33b8a
livm <livm@student.chalmers.se>
2020-11-18 16:29:22 +0100

    Boolean for checking if a dental clinic admin has signed in

---
+++ src/store/modules/dentalClinic.js
@@ -0,0 +1,31 @@
+// TODO: Import api
+
+const state = {
+  clinicSignedIn: false,
+}
+
+const getters = {
+    clinicSignedIn: state => {
+    return state.clinicSignedIn
+  },
+}
+
+const mutations = {
+  SET_CLINICSIGNEDIN: (state, bool /*, dentalClinic*/) => {
+    state.clinicSignedIn = bool /*dentalClinic*/
+  },
+}
+
+const actions = {
+  setSignedIn: ({ commit, state }, bool) => {
+    commit('SET_CLINICSIGNEDIN', bool)
+    return state.clinicSignedIn
+  },
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+}
