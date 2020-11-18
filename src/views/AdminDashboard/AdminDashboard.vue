18d5faf93cb82e56011ffe4c30017d750deaa9ac
livm <livm@student.chalmers.se>
2020-11-18 17:07:57 +0100

    Patient log component included in view

--- src/views/AdminDashboard/AdminDashboard.vue
+++ src/views/AdminDashboard/AdminDashboard.vue
@@ -2,7 +2,13 @@
   <div class="admin-dashboard">
     <admin-dashboard-sidebar />
     <div class="admin-dashboard_content">
-      <!-- TODO: Add dashboard content here -->
+      <b-container>
+        <b-row>
+          <b-col>
+            <patient-log />
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
   </div>
 </template>
@@ -10,14 +16,16 @@
 <script>
   import { mapGetters } from 'vuex'
   import AdminDashboardSidebar from '@/components/AdminDashboardSidebar/AdminDashboardSidebar'
+  import PatientLog from '@/components/PatientLog/PatientLog'
 
   export default {
     name: 'AdminDashboard',
-    components: { AdminDashboardSidebar },
+    components: { PatientLog, AdminDashboardSidebar },
 
     computed: { ...mapGetters(['clinicSignedIn']) },
 
     created() {
+      // TODO: push to a view with sign-in when one is created, instead of '/'
       if (!this.clinicSignedIn) this.$router.push('/')
     },
   }
