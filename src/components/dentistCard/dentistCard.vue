4afc1d7ad3c9e620b264f1c4dd2a1c1beaf674c8
okeyo <okeyo@student.chalmers.se>
2020-11-18 13:18:39 +0100

    Initial dentist card

---
+++ src/components/dentistCard/dentistCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-card
+      no-body
+      class="overflow-hidden"
+      style="max-width: 350px; max-height:206px"
+    >
+      <b-row no-gutters>
+        <b-col md="6">
+          <!--Placeholder image -->
+          <b-card-img
+            src="https://picsum.photos/400/400/?image=20"
+            alt="Image"
+            class="rounded-0"
+          ></b-card-img>
+        </b-col>
+        <b-col md="6">
+          <b-card-body title="">
+            <b-card-text class="font-weight-bold">
+              <br />
+
+              <!-- Placeholder -->
+              Dentist Clinic Name
+            </b-card-text>
+            <b-card-text>
+              <b-icon icon="geo-alt-fill"></b-icon>
+
+              <!--Placeholder -->
+              Street Address
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style></style>
