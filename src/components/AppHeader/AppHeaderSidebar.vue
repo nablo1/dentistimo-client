e6824d2b5f48ec28ea0fc0e196472f07f9aaaa4b
livm <livm@student.chalmers.se>
2020-11-18 13:25:16 +0100

    Sidebar to replace dropdown in AppHeader.vue

---
+++ src/components/AppHeader/AppHeaderSidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-sidebar id="app-header-sidebar" right shadow>
+    <div class="pt-4 px-3">
+      <!-- TODO: Add v-if to display different sidebar content if the user is signed in/signed out or signing in -->
+      <!-- <signed-in-content /> -->
+      <signed-out-content />
+      <!-- <signing-in-content /> -->
+    </div>
+  </b-sidebar>
+</template>
+
+<script>
+  // import SignedInContent from './SignedInContent'
+  import SignedOutContent from './SignedOutContent'
+  // import SigningInContent from './SigningInContent'
+
+  export default {
+    name: 'AppHeaderSidebar',
+    components: {
+      /*SignedInContent,*/ SignedOutContent /*, SigningInContent*/,
+    },
+  }
+</script>
+
+<style scoped></style>
