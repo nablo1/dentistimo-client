40c49ff61916d3c8589a88b48fe7ceb889dec34d
livm <livm@student.chalmers.se>
2020-11-18 16:43:24 +0100

    Current view switches back to '/' if clinic is not auth

--- src/views/AdminDashboard/AdminDashboard.vue
+++ src/views/AdminDashboard/AdminDashboard.vue
@@ -8,10 +8,18 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
   import AdminDashboardSidebar from '@/components/AdminDashboardSidebar/AdminDashboardSidebar'
+
   export default {
     name: 'AdminDashboard',
     components: { AdminDashboardSidebar },
+
+    computed: { ...mapGetters(['clinicSignedIn']) },
+
+    created() {
+      if (!this.clinicSignedIn) this.$router.push('/')
+    },
   }
 </script>
 
