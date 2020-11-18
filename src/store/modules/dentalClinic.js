077879c29b1d3f2d3756da4708c8827e92b72bd7
livm <livm@student.chalmers.se>
2020-11-18 17:17:07 +0100

    State to handle dashboard view content

--- src/store/modules/dentalClinic.js
+++ src/store/modules/dentalClinic.js
@@ -2,11 +2,15 @@
 
 const state = {
   clinicSignedIn: false,
+  dashboardView: null,
 }
 
 const getters = {
   clinicSignedIn: state => {
     return state.clinicSignedIn
+  },
+  dashboardView: state => {
+    return state.dashboardView
   },
 }
 
@@ -14,12 +18,19 @@ const mutations = {
   SET_CLINICSIGNEDIN: (state, bool /*, dentalClinic*/) => {
     state.clinicSignedIn = bool /*dentalClinic*/
   },
+  SET_DASHBOARDVIEW: (state, viewContent) => {
+    state.dashboardView = viewContent
+  },
 }
 
 const actions = {
   setSignedIn: ({ commit, state }, bool) => {
     commit('SET_CLINICSIGNEDIN', bool)
     return state.clinicSignedIn
+  },
+  setDashboardview: ({ commit, state }, viewContent) => {
+    commit('SET_DASHBOARDVIEW', viewContent)
+    return state.dashboardView
   },
 }
 
