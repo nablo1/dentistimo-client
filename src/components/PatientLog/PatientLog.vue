b1c9fd6d1a856aba49c43c49b1f3a6ddc4c58f1f
livm <livm@student.chalmers.se>
2020-11-18 17:08:06 +0100

    Base for patient log component

---
+++ src/components/PatientLog/PatientLog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="admin-patient-log">
+    <h1>
+      Clinic patient log
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PatientLog',
+  }
+</script>
+
+<style scoped></style>
