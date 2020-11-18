e8ec71d9d8100d63b7e2bdfd0f24b581cb007225
livm <livm@student.chalmers.se>
2020-11-18 17:25:31 +0100

    Base of clinic calendar settings component used in AdminDashboard.vue

---
+++ src/components/CalendarSettings/CalendarSettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="calendar-settings">
+    <h1>
+      Clinic calendar settings
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CalendarSettings',
+  }
+</script>
+
+<style scoped></style>
