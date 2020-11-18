a85dc1fd435ffe92decfdeda83aec0ca877e4800
livm <livm@student.chalmers.se>
2020-11-18 17:26:16 +0100

    Base of clinic dentist settings component used in AdminDashboard.vue

---
+++ src/components/DentistSettings/DentistSettings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dentist-settings">
+    <h1>
+      Dentist settings
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DentistSettings',
+  }
+</script>
+
+<style scoped></style>
