4991c4ecfabc93c0d993b6021fe5d35cc9341359
livm <livm@student.chalmers.se>
2020-11-21 19:27:32 +0100

    File was linted

--- src/main.js
+++ src/main.js
@@ -10,7 +10,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // Use vue-mqtt to handle the pub-sub to backend
-Vue.use(VueMqtt, 'ws://localhost:5000', {clientId: 'DentistimoClient-' + parseInt(Math.random() * 100000)})
+Vue.use(VueMqtt, 'ws://localhost:5000', {
+  clientId: 'DentistimoClient-' + parseInt(Math.random() * 100000),
+})
 
 // Use bootstrap for styling the ui
 Vue.use(BootstrapVue)
