4cacf7bee0bac8bd99e3f2a3223f83006c65497e
mehrvarz <mehrvarz@student.chalmers.se>
2020-12-13 22:09:47 +0100

    update

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bookingPage">
     <div>
       <b-row>
         <b-col class="table" md="auto">
@@ -14,26 +14,32 @@
     </div>
     <br />
     <div>
-      <table class=" table-bordered">
+      <table class="table-bordered" md="auto">
         <thead>
           <tr>
             <th>
-              <b-button scope="col" pill variant="success">8:30-9:00</b-button>
+              <b-button scope="col" squared variant="primary"
+                >8:30-9:00</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success">9:00-9:30</b-button>
+              <b-button scope="col" squared variant="primary"
+                >9:00-9:30</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success">9:30-10:00</b-button>
+              <b-button scope="col" squared variant="primary"
+                >9:30-10:00</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success"
+              <b-button scope="col" squared variant="primary"
                 >10:00-10:30</b-button
               >
             </th>
             <th>
-              <b-button scope="col" pill variant="success"
-                >10:30-11:00</b-button
+              <b-button scope="col" squared variant="primary"
+                >11:00-11:30</b-button
               >
             </th>
           </tr>
@@ -41,61 +47,72 @@
         <tbody>
           <tr>
             <th>
-              <b-button scope="col" pill variant="success"
+              <b-button scope="col" squared variant="primary"
                 >11:00-11:30</b-button
               >
             </th>
             <th>
-              <b-button scope="col" pill variant="success"
+              <b-button scope="col" squared variant="primary"
                 >11:30-12:00</b-button
               >
             </th>
             <th>
-              <b-button scope="col" pill variant="success"
+              <b-button scope="col" disabled size="sm" squared variant="primary"
                 >Lunch break</b-button
               >
             </th>
             <th>
-              <b-button scope="col" pill variant="success"
+              <b-button scope="col" squared variant="primary"
                 >13:00-13:30</b-button
               >
             </th>
             <th>
-              <b-button scope="col" pill variant="success"
+              <b-button scope="col" squared variant="primary"
                 >13:30-14:00</b-button
               >
             </th>
           </tr>
           <th>
-            <b-button scope="col" pill variant="success">14:00-14:30</b-button>
+            <b-button scope="col" squared variant="primary"
+              >14:00-14:30</b-button
+            >
           </th>
           <th>
-            <b-button scope="col" pill variant="success">14:30-15:00</b-button>
+            <b-button scope="col" squared variant="primary"
+              >14:30-15:00</b-button
+            >
           </th>
           <th>
-            <b-button scope="col" pill variant="success">Fika Break</b-button>
+            <b-button scope="col" disabled size="md" squared variant="primary"
+              >Fika Break</b-button
+            >
           </th>
           <th>
-            <b-button scope="col" pill variant="success">15:30-16:00</b-button>
+            <b-button scope="col" squared variant="primary"
+              >15:30-16:00</b-button
+            >
           </th>
           <th>
-            <b-button scope="col" pill variant="success">16:00-16:30</b-button>
+            <b-button scope="col" squared variant="primary"
+              >16:00-16:30</b-button
+            >
           </th>
 
           <tr>
             <th>
-              <b-button scope="col" pill variant="success"
+              <b-button scope="col" squared variant="primary"
                 >16:30-17:00</b-button
               >
-            </th> 
-            <th scope="col" ></th>
-            <th scope="col" ></th>
-            <th scope="col" ></th>
-            <th scope="col" ></th>
+            </th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
           </tr>
         </tbody>
       </table>
     </div>
+    <div><b-button class="book" squared variant="primary">Book</b-button></div>
   </div>
 </template>
 
@@ -117,6 +134,7 @@
 <style scoped>
   .table {
     text-align: center;
+    background-color: rgb(185, 209, 236);
   }
   .table-bordered {
     margin-left: auto;
@@ -124,5 +142,14 @@
     padding: 30px;
     border: 2px solid black;
     border-collapse: collapse;
+    background-color: rgb(185, 209, 236);
+  }
+  .book {
+    margin-left: 47%;
+    margin-top: 1cm;
+    margin-bottom: 1in;
+  }
+  .bookingPage {
+    background-color: rgb(185, 209, 236);
   }
 </style>
