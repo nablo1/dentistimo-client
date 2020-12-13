ca7cac17529c3209c98f87542e622012d0eabe43
mehrvarz <mehrvarz@student.chalmers.se>
2020-12-13 20:16:50 +0100

    Booking tabel has been changed

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -21,323 +21,77 @@
               <b-button scope="col" pill variant="success">8:30-9:00</b-button>
             </th>
             <th>
-              <b-button scope="col" pill variant="success">8:30-9:00</b-button>
+              <b-button scope="col" pill variant="success">9:00-9:30</b-button>
             </th>
             <th>
-              <b-button scope="col" pill variant="success">8:30-9:00</b-button>
+              <b-button scope="col" pill variant="success">9:30-10:00</b-button>
             </th>
             <th>
-              <b-button scope="col" pill variant="success">8:30-9:00</b-button>
+              <b-button scope="col" pill variant="success"
+                >10:00-10:30</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success">8:30-9:00</b-button>
+              <b-button scope="col" pill variant="success"
+                >10:30-11:00</b-button
+              >
             </th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <th>
-              <b-button scope="col" pill variant="success">9:00-9:30</b-button>
+              <b-button scope="col" pill variant="success"
+                >11:00-11:30</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success">9:00-9:30</b-button>
+              <b-button scope="col" pill variant="success"
+                >11:30-12:00</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success">9:00-9:30</b-button>
+              <b-button scope="col" pill variant="success"
+                >Lunch break</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success">9:00-9:30</b-button>
+              <b-button scope="col" pill variant="success"
+                >13:00-13:30</b-button
+              >
             </th>
             <th>
-              <b-button scope="col" pill variant="success">9:00-9:30</b-button>
+              <b-button scope="col" pill variant="success"
+                >13:30-14:00</b-button
+              >
             </th>
           </tr>
           <th>
-            <b-button scope="col" pill variant="success">10:00-10:30</b-button>
+            <b-button scope="col" pill variant="success">14:00-14:30</b-button>
           </th>
           <th>
-            <b-button scope="col" pill variant="success">10:00-10:30</b-button>
+            <b-button scope="col" pill variant="success">14:30-15:00</b-button>
           </th>
           <th>
-            <b-button scope="col" pill variant="success">10:00-10:30</b-button>
+            <b-button scope="col" pill variant="success">Fika Break</b-button>
           </th>
           <th>
-            <b-button scope="col" pill variant="success">10:00-10:30</b-button>
+            <b-button scope="col" pill variant="success">15:30-16:00</b-button>
           </th>
           <th>
-            <b-button scope="col" pill variant="success">10:00-10:30</b-button>
+            <b-button scope="col" pill variant="success">16:00-16:30</b-button>
           </th>
 
           <tr>
             <th>
               <b-button scope="col" pill variant="success"
-                >10:30-11:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >10:30-11:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >10:30-11:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >10:30-11:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >10:30-11:00</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:00-11:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:00-11:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:00-11:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:00-11:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:00-11:30</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:30-12:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:30-12:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:30-12:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:30-12:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >11:30-12:00</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:00-13:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:00-13:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:00-13:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:00-13:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:00-13:30</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:30-14:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:30-14:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:30-14:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:30-14:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >13:30-14:00</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:00-14:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:00-14:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:00-14:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:00-14:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:00-14:30</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:30-15:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:30-15:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:30-15:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:30-15:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >14:30-15:00</b-button
-              >
-            </th>
-          </tr>
-
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >15:30-16:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >15:30-16:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >15:30-16:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >15:30-16:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >15:30-16:00</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:00-16:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:00-16:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:00-16:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:00-16:30</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:00-16:30</b-button
-              >
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <b-button scope="col" pill variant="success"
                 >16:30-17:00</b-button
               >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:30-17:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:30-17:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:30-17:00</b-button
-              >
-            </th>
-            <th>
-              <b-button scope="col" pill variant="success"
-                >16:30-17:00</b-button
-              >
-            </th>
+            </th> 
+            <th scope="col" ></th>
+            <th scope="col" ></th>
+            <th scope="col" ></th>
+            <th scope="col" ></th>
           </tr>
         </tbody>
       </table>
